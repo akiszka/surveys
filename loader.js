64b55b4a2cf4f2617a981771e26730d0452f0bfc
Antoni Kiszka <antonikiszka@icloud.com>
2021-03-12 15:18:48 +0100

    Added 'not found' page.

--- loader.js
+++ loader.js
@@ -1,29 +1,31 @@
 function loadSurvey(id) {
+  var form = document.getElementById("mainForm")
+
   fetch('surveys/'+id+".json")
     .then(response => response.json())
     .then(data => {
-      var form = document.getElementById("mainForm")
       var htmlString = ""
 
       data.forEach((question, qIndex) => {
 	htmlString += `
 <section class="pa4 ma3 bg-white br4">
-	<p class="f3 ma3 mt1 sans-serif">${question.name}</p>
-`
+	<p class="f3 ma3 mt1 sans-serif">${question.name}</p>`
 
 	question.answers.forEach((answer, aIndex) => {
 	  htmlString += `
 <div class="pa1">
   <input type="${question.type}" id="q${qIndex}-${aIndex}" name="${qIndex}" value="${aIndex}">
   <label for="q${qIndex}-${aIndex}">${answer}</label>
-</div>
-`
+</div>`
 	})
 
 	htmlString += '</section>'
       });
 
-      htmlString += '<section class="pa4 pt1 ml3 br4"><input class="pa1 pl3 pr3" type="submit" value="Wyślij"></section>'
+      htmlString += '<section class="pa4 pt1 ml3"><input class="pa1 pl3 pr3" type="submit" value="Wyślij"></section>'
       form.innerHTML = htmlString
-    });
+    })
+    .catch(q => {
+      form.innerHTML = '<section class="pa4 mt1 bg-white br4"><h2 class="sans-serif">Nie znaleziono ankiety</h2></section>'
+    })
 }
